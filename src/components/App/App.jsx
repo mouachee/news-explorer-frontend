357d855b3243158c1d57a5c2317763d0fca3bf61
mouacheethao <thaomuajtsim005@gmail.com>
2024-10-12 00:28:50 -0500

    add handleSearch handler

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,15 +4,33 @@ import Header from "../Header/Header";
 import Main from "../Main/Main";
 import About from "../About/About";
 import Footer from "../Footer/Footer";
+import { fetchNews } from "../../utils/api";
+import { useState } from "react";
+
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSearch = async (keyword) => {
+    setLoading(true);
+    try {
+      const fetchArticles = await fetchNews(keyword);
+      setArticles(fetchArticles);
+    } catch (err) {
+      console.error(err);
+    }
+    setLoading(false);
+  };
+
   return (
     <div className="app">
       <div className="app__content">
         <div className="app__search">
           <Nav />
-          <Header />
+          <Header setSearchTerm={setSearchTerm} onSearch={handleSearch} />
         </div>
-        <Main />
+        <Main searchTerm={searchTerm} articles={articles} />
         <About />
         <Footer />
       </div>
