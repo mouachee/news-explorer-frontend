4251a4762c7b48cd91ce1fc69ecda1544757f121
mouacheethao <thaomuajtsim005@gmail.com>
2024-10-12 00:32:25 -0500

    Display fetched news data and add card click functionality

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -4,8 +4,20 @@ import noSaveIcon from "../../assets/noSaveIcon.png";
 function NewsCard({ item }) {
   return (
     <li className="card">
-      <img src={item.urlToImage} alt={item.title} className="card__image" />
-
+      <a
+        href={item.url}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="card__link"
+      >
+        <img src={item.urlToImage} alt={item.title} className="card__image" />
+        <div className="card__info">
+          <p className="card__published">{item.publishedAt.split("T")[0]}</p>
+          <h2 className="card__title">{item.title}</h2>
+          <p className="card__description">{item.description}</p>
+          <p className="card__source">{item.source.name}</p>
+        </div>
+      </a>
       <button className="card__saved--button">
         <img
           src={noSaveIcon}
@@ -14,12 +26,6 @@ function NewsCard({ item }) {
         />
         {""}
       </button>
-      <div className="card__info">
-        <p className="card__published">{item.publishedAt}</p>
-        <h2 className="card__title">{item.title}</h2>
-        <p className="card__description">{item.description}</p>
-        <p className="card__source">{item.source.name}</p>
-      </div>
     </li>
   );
 }
