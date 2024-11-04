e62d3f30e4aba314a356b83318e8f8cfb6893b4b
mouacheethao <thaomuajtsim005@gmail.com>
2024-11-04 10:39:59 -0600

    switch to Home or Save articles page

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -1,9 +1,18 @@
 import "./MobileMenu.css";
 import { Link } from "react-router-dom";
-function MobileMenu({ isOpen, onClose, isLoggedIn, onLogOut, onSignInClick }) {
+function MobileMenu({
+  isOpen,
+  onClose,
+  isLoggedIn,
+  onLogOut,
+  onSignInClick,
+  logoutIcon,
+  isHomePage,
+}) {
   if (!isOpen) return null;
   return (
     <div className="mobile-menu">
+      <div className="overlay" onClick={onClose}></div>
       <div className="mobile-menu__container">
         <ul className="mobile-menu__nav-list">
           <li className="mobile-menu__item">
@@ -16,13 +25,41 @@ function MobileMenu({ isOpen, onClose, isLoggedIn, onLogOut, onSignInClick }) {
           </li>
         </ul>
         <div className="mobile-menu__container-content">
-          <Link className="mobile-menu__home" to="/">
-            <span className="mobile-menu__text-home" onClick={onClose}>
-              {" "}
-              Home
-            </span>
-          </Link>
-          <button className="mobile-menu__signin">Sign in</button>
+          <span className="mobile-menu__text">
+            <Link
+              className="mobile-menu__link-text"
+              onClick={onClose}
+              to={isHomePage && isLoggedIn ? "/saved-news" : "/"}
+            >
+              {isHomePage && isLoggedIn ? "Saved articles" : "Home"}
+            </Link>
+          </span>
+          {isLoggedIn ? (
+            <button
+              className="mobile-menu__logout"
+              onClick={() => {
+                onLogOut();
+                onClose();
+              }}
+            >
+              <span className="moble-menu__username">Elise</span>
+              <img
+                src={logoutIcon}
+                alt="Mobile logout icon"
+                className="mobile-menu__logout-icon"
+              />
+            </button>
+          ) : (
+            <button
+              className="mobile-menu__signin"
+              onClick={() => {
+                onSignInClick();
+                onClose();
+              }}
+            >
+              Sign in
+            </button>
+          )}
         </div>
       </div>
     </div>
